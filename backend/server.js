ef2af773c9066e9458fc1cbe3626a7bf07adf061
tsering-hub <ts19sherpa@gmail.com>
2023-04-04 15:02:54 +0545

    database connection

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("Server...");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 8000;
+
+// Database connection
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
